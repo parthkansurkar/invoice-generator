4340d64b922a2afa5c80ffb72706690f68489df6
Kaumudi Shinde <kaumudishinde15704@gmail.com>
2025-10-23 20:16:44 +0530

    Template Revamp

--- invoice-generator/invoice_generator/invoice_app/views.py
+++ invoice-generator/invoice_generator/invoice_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def template_selection(request):
+    document_type = "invoice"
+    return render(request, "template_selection.html", {"document_type": document_type})
